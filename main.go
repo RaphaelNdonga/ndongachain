d2be7129c551e2b47fd98433cabba9f71be25653
RaphaelNdonga <nkibi53@gmail.com>
2022-07-22 12:53:58 +0300

    created proof of work algorithm according to a certain difficulty

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/RaphaelNdonga/ndongachain/blockchain"
 )
@@ -10,12 +11,16 @@ func main() {
 	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after genesis")
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
+	chain.AddBlock("Second Block after genesis")
+	chain.AddBlock("Third Block after genesis")
 
 	for _, block := range chain.Blocks {
 		fmt.Printf("Current Hash: %x\n", block.Hash)
 		fmt.Printf("Prev Hash: %x\n ", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("pow: %s", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
